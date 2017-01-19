8f5899730db8b023282378ec72a00ae2107363ca
zhuhh <jackjboss@gmail.com>
2017-01-19 21:48:22 +0800

    新增java实现

--- src/main/scala/com/gochinatv/spark/java/JavaRddTest.java
+++ src/main/scala/com/gochinatv/spark/java/JavaRddTest.java
@@ -1,13 +1,11 @@
 package com.gochinatv.spark.java;
 
+import com.google.common.base.Optional;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.api.java.function.*;
 import scala.Tuple2;
 
 import java.util.ArrayList;
@@ -117,23 +115,25 @@ public class JavaRddTest {
         /**************************RDD合并过滤,union、filter*********************************/
         List<Tuple2<String,Integer>> scoreList = new ArrayList<Tuple2<String, Integer>>();
         scoreList.add(new Tuple2<String, Integer>("圆钢",100));
+        scoreList.add(new Tuple2<String, Integer>("圆钢",99999));
         scoreList.add(new Tuple2<String, Integer>("张玮",98));
         scoreList.add(new Tuple2<String, Integer>("阿辉",20));
         scoreList.add(new Tuple2<String, Integer>("阿纲",29));
         scoreList.add(new Tuple2<String, Integer>("小徐",12));
         scoreList.add(new Tuple2<String, Integer>("小微",89));
+        scoreList.add(new Tuple2<String, Integer>("小微1",89));
 
 
-        List<Tuple2<String,Integer>> namesList = new ArrayList<Tuple2<String, Integer>>();
-        scoreList.add(new Tuple2<String, Integer>("圆钢",1989));
-        scoreList.add(new Tuple2<String, Integer>("张玮",1654));
-        //scoreList.add(new Tuple2<String, Integer>("阿辉",20));
-        //scoreList.add(new Tuple2<String, Integer>("阿纲",29));
-        scoreList.add(new Tuple2<String, Integer>("小徐",234));
-        scoreList.add(new Tuple2<String, Integer>("小微",185));
+        List<Tuple2<String,Integer>> moneyList = new ArrayList<Tuple2<String, Integer>>();
+        moneyList.add(new Tuple2<String, Integer>("圆钢",1989));
+        moneyList.add(new Tuple2<String, Integer>("张玮",1654));
+        moneyList.add(new Tuple2<String, Integer>("jack",20));
+        moneyList.add(new Tuple2<String, Integer>("tomcat",29));
+        moneyList.add(new Tuple2<String, Integer>("小徐",234));
+        moneyList.add(new Tuple2<String, Integer>("小微",185));
 
         JavaRDD<Tuple2<String, Integer>> score_parallelize = sc.parallelize(scoreList);
-        JavaRDD<Tuple2<String, Integer>> name_parallelize = sc.parallelize(namesList);
+        JavaRDD<Tuple2<String, Integer>> money_parallelize = sc.parallelize(moneyList);
 
         JavaPairRDD<String, Integer> pairRdd1 = score_parallelize.mapToPair(new PairFunction<Tuple2<String, Integer>, String, Integer>() {
             public Tuple2<String, Integer> call(Tuple2<String, Integer> tuple) throws Exception {
@@ -141,13 +141,51 @@ public class JavaRddTest {
             }
         });
 
-        JavaPairRDD<String, Integer> pairRdd2 = name_parallelize.mapToPair(new PairFunction<Tuple2<String, Integer>, String, Integer>() {
+        JavaPairRDD<String, Integer> pairRdd2 = money_parallelize.mapToPair(new PairFunction<Tuple2<String, Integer>, String, Integer>() {
             public Tuple2<String, Integer> call(Tuple2<String, Integer> tuple) throws Exception {
                 return new Tuple2<String, Integer>(tuple._1, tuple._2);
             }
         });
 
-        JavaPairRDD<String, Tuple2<Integer, Integer>> join = pairRdd1.join(pairRdd2);
+        /** join:   注意这里只能打印关联的上的信息,必须两个rdd都村在才能打印出来*/
+        /*JavaPairRDD<String, Tuple2<Integer, Integer>> join =pairRdd1.join(pairRdd2);
+        join.foreach(new VoidFunction<Tuple2<String, Tuple2<Integer, Integer>>>() {
+            public void call(Tuple2<String, Tuple2<Integer, Integer>> tuple) throws Exception {
+                int score = tuple._2._1;
+                int money = tuple._2._2;
+                System.out.println("姓名:"+tuple._1+" 分数:"+score+" money:"+money);
+            }
+        });*/
+        System.out.println("===========================================================");
 
+        /** leftOuterJoin:   注意这里打印出左边rdd的所有元素,右边关联上的打印,没关联上的不打印*/
+        /** (姓名:阿辉 分数:20 money:0),(姓名:圆钢 分数:100 money:1989),(姓名:圆钢 分数:99999 money:1989)    */
+        JavaPairRDD<String, Tuple2<Integer, Optional<Integer>>> leftOutJoin = pairRdd1.leftOuterJoin(pairRdd2);
+        leftOutJoin.foreach(new VoidFunction<Tuple2<String, Tuple2<Integer, Optional<Integer>>>>() {
+            public void call(Tuple2<String, Tuple2<Integer, Optional<Integer>>> tuple) throws Exception {
+                int score = tuple._2._1;
+                int money = tuple._2._2.or(0);
+                System.out.println("姓名:"+tuple._1+" 分数:"+score+" money:"+money);
+            }
+        });
+
+        System.out.println("===========================================================");
+
+        /** leftOuterJoin:   注意这里打印出右边rdd的所有元素,左边关联上的打印,没关联上的不打印*/
+        /*JavaPairRDD<String, Tuple2<Optional<Integer>, Integer>> rightOutJoin = pairRdd1.rightOuterJoin(pairRdd2);
+        rightOutJoin.foreach(new VoidFunction<Tuple2<String, Tuple2<Optional<Integer>,Integer>>>() {
+            public void call(Tuple2<String, Tuple2<Optional<Integer>,Integer>> tuple) throws Exception {
+                int score = tuple._2._1.or(0);
+                int money = tuple._2._2;
+                System.out.println("姓名:"+tuple._1+" 分数:"+score+" money:"+money);
+            }
+        });*/
+
+
+
+        /** cogroup :  当出现相同Key时, join会出现笛卡尔积, 而cogroup的处理方式不同*/
+        /** (阿纲,([29],[])), (圆钢,([100, 99999],[1989]))  */
+        JavaPairRDD<String, Tuple2<Iterable<Integer>, Iterable<Integer>>> cogroup = pairRdd1.cogroup(pairRdd2);
+        System.out.println(cogroup.collect().toString());
     }
 }
