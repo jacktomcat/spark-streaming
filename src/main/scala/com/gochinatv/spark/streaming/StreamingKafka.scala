72b676baa787c191cc89469fac679286406fe65f
zhuhuihui <jackjboss@gmail.com>
2017-03-24 17:39:50 +0800

    新增kafka 数据流

--- src/main/scala/com/gochinatv/spark/streaming/StreamingKafka.scala
+++ src/main/scala/com/gochinatv/spark/streaming/StreamingKafka.scala
@@ -15,8 +15,12 @@ object StreamingKafka {
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("StreamingKafka").setMaster("local[*]")
     val ssc = new StreamingContext(sparkConf, Seconds(10))
-    val inputStream = KafkaUtils.createStream(ssc,"192.168.2.150:2181","spark-test-01", Map[String, Int]("spark-test" -> 1))
+
+    //收到消息格式 (key237,{"id":"236","ts":"2017-03-24 17:34:29","count":"9","value":"39","agreeid":"323"})
+    val inputStream = KafkaUtils.createStream(ssc,"localhost:2181","streaming-g01", Map[String, Int]("streaming-click" -> 1))
     inputStream.print()
+
+
     //KafkaUtils.createDirectStream()
 
     ssc.start()
